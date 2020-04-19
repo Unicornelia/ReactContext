ade0dd1378cebdd2fd0a61936ae7219d4a7e837c
Kori <korneliaszabo@kinsta.com>
2020-04-19 17:17:14 +0200

    Update content of index.js and create api.js to follow lecture (egghead.io)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import ReactDOM from "react-dom";
+import LoginPage from './LoginPage';
+import MainPage from './MainPage';
+import UserContext from './UserContext';
+import { FAKE_USER } from './api';
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import * as serviceWorker from "./serviceWorker";
+
+
+class Root extends Component {
+  state = {
+    currentUser = FAKE_USER
+  };
+
+  handleLogin = user => {
+    this.setState({currentUser: user})
+  }
+
+  handleLogout = user => {
+    this.setState({currentUser: null})
+  }
+
+  render() {
+    return this.state.currentUser ? (
+      <UserContext.Provider value={{this.state.currentUser}}>
+        <MainPage onLogout={this.handleLogout}/>
+      </UserContext.Provider>
+    ) : (
+      <LoginPage onLogin={this.handleLogin}/>
+    )
+  }
+}
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
